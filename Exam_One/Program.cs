fef73d1fd6f92a56ee58bcabcd7b1621b699d882
Extro <22ff2296@gmail.com>
2020-06-20 15:39:44 +0500

    Добавьте файлы проекта.

---
+++ Exam_One/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exam_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int word = 0;
+            string[] text = File.ReadAllLines("file.txt");
+            for (int i = 0; i < text.Length; i++)
+            {
+                string[] mass;
+                mass = text[i].Split(' ');
+                for (int c = 0; c < mass.Length; c++)
+                {
+                    if (mass[c] == "STUDENT")
+                    {
+                        word++;
+                    }
+                }
+            }
+            Console.WriteLine(word);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exam_One/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Exam_One")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exam_One")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("052a491a-ca5f-4eb9-ac35-d5a4706bb368")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
